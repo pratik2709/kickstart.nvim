28184cb015dad5bb81bd87779f9608f8c3819f9a
Pratik Saxena <pratik2709@gmail.com>
2024-12-30 21:08:53 +0530

    added theme

---
+++ lua/custom/plugins/nostrotheme.lua
@@ -0,0 +1,19 @@
+return {
+  'meanderingexile/nostromo-ui.nvim',
+  config = function()
+    -- Optional: set up theme-specific configurations
+    require('nostromo-ui').setup {
+      -- (note: if your configuration sets vim.o.background the following option will do nothing!)
+      theme = 'dark', -- String: 'dark' or 'light', determines the colorscheme used
+      transparent = false, -- Boolean: Sets the background to transparent
+      italics = {
+        comments = true, -- Boolean: Italicizes comments
+        keywords = true, -- Boolean: Italicizes keywords
+        functions = true, -- Boolean: Italicizes functions
+        strings = true, -- Boolean: Italicizes strings
+        variables = true, -- Boolean: Italicizes variables
+      },
+      overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+    }
+  end,
+}
