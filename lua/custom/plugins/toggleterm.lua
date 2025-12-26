b52cd46c04b49fa26967d9668957333f7b3a15f5
Pratik Saxena <pratik2709@gmail.com>
2025-12-26 22:41:40 +0530

    added toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     direction = 'horizontal', -- or 'vertical' / 'float'
     size = 15,
-    open_mapping = [[<c-\>]], -- This sets Ctrl+\ to open/hide it
+    --    open_mapping = [[<c-\>]], -- This sets Ctrl+\ to open/hide it
     shading_factor = 2,
     float_opts = {
       border = 'curved', -- Gives it that modern "depth" look
