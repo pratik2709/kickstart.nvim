e33e1e820124164dac6f39a3cb114d559ebabccb
Pratik Saxena <pratik2709@gmail.com>
2025-12-26 19:59:58 +0530

    toggleterm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    direction = 'horizontal', -- or 'vertical' / 'float'
+    size = 15,
+    open_mapping = [[<c-\>]], -- This sets Ctrl+\ to open/hide it
+    shading_factor = 2,
+    float_opts = {
+      border = 'curved', -- Gives it that modern "depth" look
+    },
+  },
+}
