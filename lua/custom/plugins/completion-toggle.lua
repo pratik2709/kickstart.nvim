bf949803cd328ecc503fe97a8b5eb8d21c319daf
Pratik Saxena <pratik2709@gmail.com>
2026-02-14 18:06:46 +1300

    completion toggle

--- lua/custom/plugins/completion-toggle.lua
+++ lua/custom/plugins/completion-toggle.lua
@@ -1,39 +1,23 @@
 -- Completion toggle for blink.cmp
 -- Adds <leader>tc to toggle autocomplete on/off (useful when learning a language)
 
+vim.g.blink_cmp_enabled = true
+
+vim.keymap.set('n', '<leader>tc', function()
+  vim.g.blink_cmp_enabled = not vim.g.blink_cmp_enabled
+
+  if vim.g.blink_cmp_enabled then
+    vim.notify('Autocomplete enabled', vim.log.levels.INFO)
+  else
+    vim.notify('Autocomplete disabled', vim.log.levels.INFO)
+  end
+end, { desc = '[T]oggle [C]ompletion' })
+
 return {
   'saghen/blink.cmp',
-  keys = {
-    {
-      '<leader>tc',
-      function()
-        local enabled = vim.g.blink_cmp_enabled
-        if enabled == nil then
-          enabled = true -- default state is enabled
-        end
-        vim.g.blink_cmp_enabled = not enabled
-
-        if vim.g.blink_cmp_enabled then
-          vim.notify('Autocomplete enabled', vim.log.levels.INFO)
-        else
-          vim.notify('Autocomplete disabled', vim.log.levels.INFO)
-        end
-      end,
-      desc = '[T]oggle [C]ompletion',
-    },
-  },
   opts = {
-    completion = {
-      menu = {
-        enabled = function()
-          -- Check if autocomplete is globally disabled
-          local enabled = vim.g.blink_cmp_enabled
-          if enabled == nil then
-            return true -- default is enabled
-          end
-          return enabled
-        end,
-      },
-    },
+    enabled = function()
+      return vim.g.blink_cmp_enabled
+    end,
   },
 }
