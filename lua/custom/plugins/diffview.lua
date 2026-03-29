84095d708b232764e5deeb3772155372ddba7c2f
Pratik Saxena <pratik2709@gmail.com>
2026-03-29 14:26:38 +1300

    added diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,3 +1,7 @@
 return {
   'sindrets/diffview.nvim',
+  keys = {
+    { '<leader>do', '<cmd>DiffviewOpen<cr>', desc = 'Diffview open' },
+    { '<leader>dc', '<cmd>DiffviewClose<cr>', desc = 'Diffview close' },
+  },
 }
