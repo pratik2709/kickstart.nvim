78a4d8bed0beb8a43b08ebfbec78cb55fafe5e0c
Pratik Saxena <pratik2709@gmail.com>
2026-09-12 14:42:43 +1200

    annotate changes

--- lua/custom/plugins/annotate.lua
+++ lua/custom/plugins/annotate.lua
@@ -3,6 +3,31 @@ return {
   config = function()
     require('annotate').setup {}
 
+    local annotation_width = 0.8
+    local annotation_height = 15
+
+    -- annotate.nvim currently hardcodes its floating-window dimensions.
+    -- Resize only its annotation buffers so other floating windows are unaffected.
+    vim.api.nvim_create_autocmd('BufWinEnter', {
+      pattern = 'annotate://*',
+      callback = function(args)
+        local win = vim.fn.bufwinid(args.buf)
+        if win == -1 then
+          return
+        end
+
+        local width = math.floor(vim.o.columns * annotation_width)
+        local height = annotation_height
+        vim.api.nvim_win_set_config(win, {
+          relative = 'editor',
+          width = width,
+          height = height,
+          row = math.floor((vim.o.lines - height) / 2),
+          col = math.floor((vim.o.columns - width) / 2),
+        })
+      end,
+    })
+
     local store = require 'annotate.store'
     local export = require 'annotate.export'
     local original_save = store.save
